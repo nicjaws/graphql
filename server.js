c3ae1c24ba2b5303d2a5754c664b3b5cdab22967
Nicolas Corbalan <outreef@hotmail.com>
2021-07-29 11:10:06 +0200

    starting graphiql

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const app = express();
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList,
+} = require("graphql");
 
-app.use("graphql", graphqlHTTP({
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: () => ({
+      message: {type: GraphQLString, resolve:() => "Hello World"}
+  })
+ }),
+});
+
+app.use("/graphql", graphqlHTTP({
+  schema: schema,
   graphiql: true
 }));
 
